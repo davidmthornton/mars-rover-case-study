c9ca545a9bedb3cf68fb117930557eeff8eba5fc
martinarmstrong <martin.armstrong@digital.hmrc.gov.uk>
2021-03-15 10:11:46 +0000

    wip

--- src/main/scala/code/Main.scala
+++ src/main/scala/code/Main.scala
@@ -1,5 +1,25 @@
 package code
 
+
 object Main extends App {
-  println("Mars, here we come!")
+
+  sealed trait Terrain
+  case object Sand extends Terrain {
+    override def toString() = {"."}
+  }
+  case object Rock extends Terrain {
+    override def toString() = {"O"}
+  }
+
+  case class Grid(rows:Vector[Vector[Terrain]]) {
+    override def toString() = {
+      rows.map(_.mkString).mkString("\n")
+    }
+  }
+
+
+  def getDefaultGrid():Grid = {
+    Grid(Vector.tabulate(5,5)((_,_)=>Sand))
+  }
+
 }
--- src/test/scala/code/MainSpec.scala
+++ src/test/scala/code/MainSpec.scala
@@ -4,9 +4,13 @@ import org.scalatest.wordspec.AnyWordSpec
 import org.scalatest.matchers.should.Matchers
 
 class MainSpec extends AnyWordSpec with Matchers {
-  "true" should {
-    "equal false" in {
-      true shouldBe false
+  "getdefaultGrid" should {
+    "be a 5x5 grid of Sand" in {
+      Main.getDefaultGrid().toString() shouldBe ("""|.....
+        |.....
+        |.....
+        |.....
+        |.....""").stripMargin
     }
   }
 }
