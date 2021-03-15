24f3b00791e527ead770c53fe924ce6c2bb2bc2e
David Thornton <28890110+davidmthornton@users.noreply.github.com>
2021-03-15 12:29:45 +0000

    Where we got upto

--- src/main/scala/code/models/Position.scala
+++ src/main/scala/code/models/Position.scala
@@ -1,3 +1,14 @@
 package code.models
 
-case class Position(column: Int, row: Int)
+case class Position(column: Int, row: Int) {
+
+  def rotate(direction: Direction): Position = {
+    direction match {
+      case North => this.copy(row = this.row - 1)
+      case East => this.copy(column = this.column + 1)
+      case South => this.copy(row = this.row + 1)
+      case West => this.copy(column = this.column - 1)
+    }
+  }
+
+}
--- src/main/scala/code/models/Rover.scala
+++ src/main/scala/code/models/Rover.scala
@@ -1,9 +1,11 @@
 package code.models
 
+import code.models.Rover.Command
+
 import java.rmi.activation.ActivationGroupDesc.CommandEnvironment
+import scala.util.Try
 
 case class Rover(direction: Direction, position: Position) {
-//  def move(direction: Direction)
 
   sealed trait Error
   case object WillFallOffPlanet extends Error
@@ -18,11 +20,20 @@ case class Rover(direction: Direction, position: Position) {
     (command,grid) match {
       case (Rover.Left, _) => Right(this.copy(direction = this.direction.leftOf))
       case (Rover.Right, _) => Right(this.copy(direction = this.direction.rightOf))
+      case (Rover.Forward, _) => move(position, Rover.Forward, grid)
       case _ => Left(Unknown)
     }
   }
 
+  def move(startPosition: Position, direction: Command, grid: Grid): Either[Error, Rover] = {
+    val newPosition: Position = startPosition.(direction)
+    val newTerrain: Try[Terrain] = grid.getTerrain(startPosition)
+
+    if(newTerrain.isSuccess) Right(this.copy(position = newPosition)) else Left(WillFallOffPlanet)
+  }
+
 }
+
 object Rover {
   sealed trait Command
   case object Left extends Command
